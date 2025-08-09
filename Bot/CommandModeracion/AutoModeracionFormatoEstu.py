4f46c9fdfa591c7e3c25c43a4ab4215e6b1d420f
Jessua Solis <147453642+Jessuapapu@users.noreply.github.com>
2025-08-08 21:55:51 -0600

    Singleton asyncrono para las views persistentes

--- Bot/CommandModeracion/AutoModeracionFormatoEstu.py
+++ Bot/CommandModeracion/AutoModeracionFormatoEstu.py
@@ -1,4 +1,4 @@
-from discord import Interaction, Color
+from discord import Interaction, Color, File
 from Clases.Botones import BotonBase
 from Clases import util
 from Declaraciones import views
@@ -11,8 +11,10 @@ async def crear_boton_registro(interaction: Interaction):
     
     mensajeEmbed = "Holaaa, Te damos la bienvenida al servidor de discord del curso de CS50x.ni :kissing_smiling_eyes:\n\nEste es un simple paso de autentificacion!!!, solo te pediremos tu nombre completo y tu usuario del docs (el usuario con el que entras a ver los psets :nerd:)\n\n ¿tienes problemas para auntentificarte? Contacta a un staff"
     mensajeEmbed = util.CrearMensajeEmbed("Hola, esto es un simple registro :disguised_face:",mensajeEmbed, Color.gold())
-    
+    imagen_embed = File("./Bot/Plantilla/Registro Automatico.jpg")
+    mensajeEmbed.set_image(url='attachment://imagen_embed.png')
     await interaction.response.send_message("Creando...")    
-    await interaction.channel.send(embed=mensajeEmbed, view=VPS.vistaRegistro)
+    await interaction.channel.send(embed=mensajeEmbed, view=VPS.vistaRegistro, file= imagen_embed)
+    
 
     
